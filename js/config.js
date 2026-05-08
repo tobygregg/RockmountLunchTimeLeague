be58d63692c6076778c92c22463258f4cf2eb132
tobygregg <toby1@greggs.org>
2026-05-08 15:07:05 +0100

    Add configuration for league points and statistics

---
+++ js/config.js
@@ -0,0 +1,32 @@
+const CONFIG = {
+  SHEET_ID: "1MKzj6fGVqAwzKjS0qXVnMj8mlust_amG_s3-OoJDO3A",
+  SHEET_NAME: "SFC Rockmount League",
+
+  // Current League Points
+  SylvansPoints: "E6",
+  CPFCPoints: "G6",
+
+  // Total Wins
+  SylvansWins: "F12",
+  CPFCWins: "G12",
+
+  // Total Goals
+  SylvansGoals: "B12",
+  CPFCGoals: "C12",
+
+  // Win Rate
+  SylvansWinRate: "F13",
+  CPFCWinRate: "G13",
+
+  // Draws
+  Draws: "H12",
+
+  // Latest Match
+  LatestMatchDate: "J6",
+  LatestSylvansScore: "K6",
+  LatestCPFCScore: "L6",
+  LatestMOTM: "M6",
+
+  // History Range
+  HistoryRange: "B26:E110"
+};
